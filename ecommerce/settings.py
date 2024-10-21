44e35d93684496b18a7a33d5a84b6442c10ffcb5
MariamChtioui <chtiouimariam17@gmail.com>
2024-10-21 20:48:26 +0100

    Message de commit

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -27,7 +27,8 @@ SECRET_KEY = '#vw(03o=(9kbvg!&2d5i!2$_58x@_-3l4wujpow6(ym37jxnza'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app']
+ALLOWED_HOSTS = ['web-ecommerce-hopau11iq-mariams-projects-c5d51f28.vercel.app']
+
 
 
 # Application definition
